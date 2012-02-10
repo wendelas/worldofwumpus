88c33cc444e2e4086a7a71a187fe9d18aa50d402
ggzaery@gmail.com <ggzaery@gmail.com@f3f4b768-d291-5a01-5aa7-5c45901c42b8>
2012-02-10 16:24:58 +0000

    rework

---
+++ trunk/src/propositional_logic/NewJFrame.java
@@ -0,0 +1,28 @@
+package propositional_logic;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+public class NewJFrame extends javax.swing.JFrame {
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+	
+	public NewJFrame() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+
+}
